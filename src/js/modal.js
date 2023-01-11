e6f61578dfad667cda19900cb3b04e3f80991b46
DeveloperStevenMarin <developerstevenmarin@gmail.com>
2023-01-11 15:00:16 -0500

    Modal form fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,45 +1,46 @@
 (() => {
     const refs = {
+      /////////////////////////////////////////////////////////////// Menu
       openModalBtn: document.querySelector("[modal-menu-open]"),
       closeModalBtn: document.querySelector("[modal-menu-close]"),
       modal: document.querySelector("[modal-menu]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModalMenu);
-    refs.closeModalBtn.addEventListener("click", toggleModalMenu);
-  
-    function toggleModalMenu() {
-      refs.modal.classList.toggle("modal--menu-is-hidden");
-     
-    }
-})();
-  
-(() => {
-    const refs = {
+
+      /////////////////////////////////////////////////////////////// Map
       openModalBtnmap: document.querySelector("[modal-map-open]"),
       closeModalBtnmap: document.querySelector("[modal-map-close]"),
       modalmap: document.querySelector("[modal-map]"),
+
+      /////////////////////////////////////////////////////////////// Form
+      openformModalBtn: document.querySelector("[modal-form-open]"),
+      closeformModalBtn: document.querySelector("[modal-form-close]"),
+      modalform: document.querySelector("[modal-form]"),
     };
-  
+    /////////////////////////////////////////////////////////////// Menu
+    refs.openModalBtn.addEventListener("click", toggleModalMenu);
+    refs.closeModalBtn.addEventListener("click", toggleModalMenu);
+
+    ////////////////////////////////////////////////////////////// Map
     refs.openModalBtnmap.addEventListener("click", toggleModalMap);
     refs.closeModalBtnmap.addEventListener("click", toggleModalMap);
-  
+
+    ////////////////////////////////////////////////////////////// Form
+    refs.openformModalBtn.addEventListener("click", toggleModalForm);
+    refs.closeformModalBtn.addEventListener("click", toggleModalForm);
+
+
+
+    /////////////////////////////////////////////////////////////// Menu
+    function toggleModalMenu() {
+      refs.modal.classList.toggle("modal--menu-is-hidden");
+    }
+
+    ////////////////////////////////////////////////////////////// Map
     function toggleModalMap() {
       refs.modalmap.classList.toggle("modal--map-is-hidden");
     }
-})();
 
-(() => {
-  const refs = {
-    openformModalBtn: document.querySelector("[modal-form-open]"),
-    closeformModalBtn: document.querySelector("[modal-form-close]"),
-    modalform: document.querySelector("[modal-form]"),
-  };
-
-  refs.openformModalBtn.addEventListener("click", toggleModalForm);
-  refs.closeformModalBtn.addEventListener("click", toggleModalForm);
-
-  function toggleModalForm() {
-    refs.modalform.classList.toggle("modal--form-is-hidden");
-  }
+    ////////////////////////////////////////////////////////////// Form
+    function toggleModalForm() {
+      refs.modalform.classList.toggle("modal--form-is-hidden");
+    }
 })();
