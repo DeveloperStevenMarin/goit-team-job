ad29dafa93fdf026cedad32a070ce09eab78c0a6
MoiMarquez <96758329+MoiMarquez@users.noreply.github.com>
2023-01-11 03:00:37 -0500

    Maps modal and return home button added

--- src/js/modal.js
+++ src/js/modal.js
@@ -11,4 +11,20 @@
     function toggleModal() {
       refs.modal.classList.toggle("modal--is-hidden");
     }
+})();
+  
+(() => {
+    const refs = {
+      openModalBtnmap: document.querySelector("[data-modal-openmap]"),
+      closeModalBtnmap: document.querySelector("[data-modal-closemap]"),
+      modalmap: document.querySelector("[data-modalmap]"),
+    };
+  
+    refs.openModalBtnmap.addEventListener("click", toggleModal);
+    refs.closeModalBtnmap.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modalmap.classList.toggle("is-hiddenmap");
+    }
   })();
+  
